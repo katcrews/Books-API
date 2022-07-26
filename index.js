9a745b1ea3a5bc92374d714267315755b9afa73e
Kat Crews <katfreitas808@gmail.com>
2022-07-26 12:18:36 -0700

    add 404 page

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
 
-
+//index
 app.get('/', function(req, res){
     res.send('hello world')
 });
 
+//404
+app.get('*', (req, res) => {
+    res.status(404).send('404 Not Found')
+})
 
-app.listen(3009, function () {
+
+app.listen(process.env.PORT, function () {
     console.log('I am awake!')
   })
   
