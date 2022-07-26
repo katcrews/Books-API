6832d15faec91973288c98a9da70128a54febaf7
Kat Crews <katfreitas808@gmail.com>
2022-07-26 12:12:50 -0700

    inital commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+
+
+app.get('/', function(req, res){
+    res.send('hello world')
+});
+
+
+app.listen(3009, function () {
+    console.log('I am awake!')
+  })
+  
